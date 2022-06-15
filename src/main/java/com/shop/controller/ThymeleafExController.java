51cb32f72a48ef81e6675912ed4e2038aa29daa7
username <user@email.mail>
2022-06-16 08:55:14 +0900

    Initial commit

---
+++ src/main/java/com/shop/controller/ThymeleafExController.java
@@ -0,0 +1,10 @@
+package com.shop.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value = "/thymeleaf")
+public class ThymeleafExController {
+
+}
